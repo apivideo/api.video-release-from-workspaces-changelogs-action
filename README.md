5267218b7cab279892d68351d15f7b6617897b7e
Olivier Lando <olivier@api.video>
2022-05-04 14:46:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+[![badge](https://img.shields.io/twitter/follow/api_video?style=social)](https://twitter.com/intent/follow?screen_name=api_video) &nbsp; [![badge](https://img.shields.io/github/stars/apivideo/api.video-release-from-workspaces-changelogs-action?style=social)](https://github.com/apivideo/api.video-release-from-workspaces-changelogs-action) &nbsp; [![badge](https://img.shields.io/discourse/topics?server=https%3A%2F%2Fcommunity.api.video)](https://community.api.video)
+![](https://github.com/apivideo/API_OAS_file/blob/master/apivideo_banner.png)
+<h1 align="center">release-from-workspaces-changelogs-action</h1>
+
+[api.video](https://api.video) is the video infrastructure for product builders. Lightning fast video APIs for integrating, scaling, and managing on-demand & low latency live streaming features in your app.
+
+# Table of contents
+
+- [Table of contents](#table-of-contents)
+- [Project description](#project-description)
+- [Documentation](#documentation)
+  - [Expected CHANGELOG file format](#expected-changelog-file-format)
+  - [Inputs](#inputs)
+      - [`github-auth-token`](#github-auth-token)
+  - [Example usage](#example-usage)
+
+# Project description
+
+Automatically create draft releases depending on the CHANGELOG.md files of each npm workspace of the repo.
+
+# Documentation
+
+## Expected CHANGELOG file format
+```markdown
+## [0.1.0] - 2021-12-06
+- Change 1
+- Change 2
+- ...
+- Change n
+
+## [0.0.1] - 2021-11-15
+- Change 1
+- ...
+
+...
+```
+
+## Inputs
+
+#### `github-auth-token`
+
+**required** GitHub authentication token.
+
+## Example usage
+
+```yml
+uses: apivideo/api.video-release-from-workspaces-changelogs-action
+with:
+  github-auth-token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,152 @@
+const fs = require('fs');
+const readline = require('readline');
+const semver = require('semver')
+const core = require('@actions/core');
+const { Octokit, App } = require("octokit");
+const github = require('@actions/github');
+const glob = require("glob")
+
+async function getVersionsFromChangelog(changelogFilePath) {
+    const fileStream = fs.createReadStream(changelogFilePath);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let currentVersion;
+    const versions = [];
+    for await (const line of rl) {
+        const versionRegexResult = /.*\s+\[v?([\d\.]+)\].*/.exec(line);
+        if (versionRegexResult) {
+            currentVersion = versionRegexResult[1];
+            versions[currentVersion] = [];
+            continue;
+        }
+
+        if (!currentVersion) {
+            continue;
+        }
+
+        const detailsRegexResult = /\s?-\s?([^\s].*)/.exec(line);
+        if (detailsRegexResult) {
+            versions[currentVersion].push(detailsRegexResult[1]);
+        }
+    }
+    return versions;
+}
+
+function listWorkspaces(path) {
+    return new Promise((resolve, reject) => glob(path, (err, files1) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(files1
+                .map((f) => {
+                    const changeLogPath = f + "/CHANGELOG.md";
+                    const packageJsonPath = f + "/package.json";
+
+                    if (fs.existsSync(changeLogPath) && fs.existsSync(packageJsonPath)) {
+                        let packageName = JSON.parse(fs.readFileSync(packageJsonPath)).name;
+                        if (packageName && packageName.indexOf("@") === 0) {
+                            packageName = packageName.substring(1);
+                        }
+                        return ({
+                            package: packageName,
+                            changelog: changeLogPath,
+                        });
+                    }
+                }).filter(a => a !== undefined));
+        }
+    }));
+}
+
+async function findNpmWorkspacesChangelogs(changelogFilePath) {
+    let packageJson;
+    try {
+        packageJson = JSON.parse(fs.readFileSync("package.json"));
+    } catch (e) {
+        throw new Error(`Could not read package.json: ${e}`);
+    }
+
+    if (!packageJson.workspaces || packageJson.workspaces.length === 0) {
+        throw new Error(`No workspace found in package.json`);
+    }
+
+    const res = await Promise.all(packageJson.workspaces.map(w => listWorkspaces(w)));
+
+    let files = [];
+    res.forEach(rr => {
+        rr.forEach(r => {
+            if (!files.find(f => f.changelog === r.changelog && f.package === r.package)) {
+                files = files.concat(r);
+            }
+        });
+    });
+    return files;
+}
+
+(async () => {
+    try {
+        const githubAuthToken = core.getInput('github-auth-token', { required: true });
+
+        const octokit = github.getOctokit(githubAuthToken);
+
+        const allReleases = (await octokit
+            .paginate(octokit.rest.repos.listReleases, {
+                repo: github.context.repo.repo,
+                owner: github.context.repo.owner,
+            })).map(a => ({
+                name: a.name,
+                draft: a.draft,
+                id: a.id,
+            }));
+
+
+
+        const changelogs = await findNpmWorkspacesChangelogs("CHANGELOG.md");
+        changelogs.forEach(async (c) => {
+            const changelogVersions = await getVersionsFromChangelog(c.changelog);
+            const lastChangelogVersion = Object.keys(changelogVersions)[0];
+            const lastChangelogDetails = changelogVersions[lastChangelogVersion];
+
+            const versionName = c.package + "@" + lastChangelogVersion;
+
+            const lastPackageRelease = allReleases.filter((release) => release.name.indexOf(c.package) === 0)[0];
+            const lastPackageReleaseName = lastPackageRelease ? lastPackageRelease.name.substring(lastPackageRelease.name.indexOf("@")) : undefined;
+            const lastPackageReleaseVersion = lastPackageRelease ? lastPackageReleaseName.split("@")[1] : undefined;
+
+            if (!lastPackageReleaseVersion || semver.lte(lastPackageReleaseVersion, lastChangelogVersion)) {
+                if (lastPackageReleaseVersion && semver.eq(lastPackageReleaseVersion, lastChangelogVersion)) {
+                    if (lastPackageRelease.draft) {
+                        await octokit.rest.repos.deleteRelease({
+                            repo: github.context.repo.repo,
+                            owner: github.context.repo.owner,
+                            release_id: lastPackageRelease.id,
+                        });
+                        core.info(`Draft release ${versionName} already exists. Replacing it.`);
+                    } else {
+                        core.info(`The release ${versionName} already exists. Nothing will be done.`);
+                        return;
+                    }
+                }
+            }
+
+            const payload = {
+                repo: github.context.repo.repo,
+                owner: github.context.repo.owner,
+                draft: true,
+                prerelease: false,
+                body: lastChangelogDetails.map(s => "- " + s).join("\n"),
+                tag_name: versionName,
+                name: versionName,
+            };
+            await octokit.rest.repos.createRelease(payload);
+            core.info(`Draft release ${versionName} has been created.`)
+        });
+
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
